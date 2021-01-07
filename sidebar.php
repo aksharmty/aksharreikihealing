92c0606ba5262573fe2afec4b6c7ceccc0ea169f
aksharmty <aksharmty@ymail.com>
2021-01-07 13:58:38 +0530

    Update and rename sidebar.php to sidebar.html

--- sidebar.php
+++
@@ -1,2 +0,0 @@
-<center><h1>Advertisement</h1></center>
-<iframe data-aa="1047743" src="//ad.a-ads.com/1047743?size=300x250" scrolling="no" style="width:300px; height:250px; border:0px; padding:0; overflow:hidden" allowtransparency="true"></iframe>
